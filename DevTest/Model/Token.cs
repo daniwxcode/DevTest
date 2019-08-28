496c4b0400c83a9c1350906ac084e3c8beec367a
Daniel WOAGOU <daniwx@gmail.com>
2019-08-28 15:38:47 +0000

    Token Summary

--- DevTest/Model/Token.cs
+++ DevTest/Model/Token.cs
@@ -7,12 +7,19 @@ namespace DevTest.Model
 {
    public  class Token
     {
+        /// <summary>
+        /// token that will be used in subsequent requests
+        /// </summary>
         [JsonProperty("access_token ")]
         public string AccessToken { get; set; }
-
+        /// <summary>
+        /// Examples: "bearer"
+        /// </summary>
         [JsonProperty("token_type ")]
         public string TokenType { get; set; }
-
+        /// <summary>
+        /// date of expiration
+        /// </summary>
         [JsonProperty("expires_at ")]
         public DateTime ExpiresAt { get; set; }
     }
